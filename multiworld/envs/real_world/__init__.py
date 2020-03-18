dd89acb5944e1625675fb207b2659fdfa08a5a3e
tunglm <tunglm2203@kaist.ac.kr>
2020-03-18 17:19:27 +0900

    Add to env Medium and Harder for Pusher, based on multiworld environment

--- multiworld/envs/real_world/__init__.py
+++ multiworld/envs/real_world/__init__.py
@@ -50,6 +50,38 @@ def register_real_worl_envs():
     )
 
     register(
+        id='SawyerPushXYRealMedium-v0',
+        entry_point='multiworld.envs.real_world.sawyer.sawyer_pushing:SawyerPushXYEnv',
+        kwargs={
+            'fix_goal': False,  # True
+            # 'fixed_goal': (.50, -0.1, .45, .0),
+            'action_mode': 'position',
+            'max_speed': max_speed,
+            'config_name': 'tung_config',
+            'hand_goal_low': (0.55, -0.05),
+            'hand_goal_high': (0.65, 0.05),
+            'puck_goal_low': (0.5, -0.2),
+            'puck_goal_high': (0.7, 0.2)
+        },
+    )
+
+    register(
+        id='SawyerPushXYRealHarder-v0',
+        entry_point='multiworld.envs.real_world.sawyer.sawyer_pushing:SawyerPushXYEnv',
+        kwargs={
+            'fix_goal': False,  # True
+            # 'fixed_goal': (.50, -0.1, .45, .0),
+            'action_mode': 'position',
+            'max_speed': max_speed,
+            'config_name': 'tung_config',
+            'hand_goal_low': (0.5, -0.2),
+            'hand_goal_high': (0.7, 0.2),
+            'puck_goal_low': (0.5, -0.2),
+            'puck_goal_high': (0.7, 0.2)
+        },
+    )
+
+    register(
         id='SawyerDoorReal-v0',
         entry_point='multiworld.envs.real_world.sawyer.sawyer_door:SawyerDoorEnv',
         kwargs={
